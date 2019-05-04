870ad95ef24b2ece74d4ae492cf42f6bdf607a19
Ali Mustakim <alim@forwardcomputers.com>
2019-05-04 09:50:06 -0500

    updates [skip ci]

--- bin/dapp-completion.bash
+++ bin/dapp-completion.bash
@@ -1,3 +1,9 @@
 #!/bin/bash
 #
+#DAPP_COMMANDS=$( grep -E '^[a-zA-Z_-]+.*{.*?## .*$' bin/dapp | sort | awk 'BEGIN {FS = "\\(.*?## "}; { printf $1 }' )
+_dapp()
+{
+  COMPREPLY=( $( compgen -W "$( grep -E '^[a-zA-Z_-]+.*{.*?## .*$' bin/dapp | awk 'BEGIN { FS = "\\(.*?## " }; { printf $1 }' )" ) )
+}
 
+complete -F _dapp dapp
