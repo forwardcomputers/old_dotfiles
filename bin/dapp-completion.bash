2e4d5559a2e1352265cd574c4eef277d88d3eb23
Ali Mustakim <alim@forwardcomputers.com>
2019-05-06 18:46:40 -0500

    updates [skip ci]

--- bin/dapp-completion.bash
+++ bin/dapp-completion.bash
@@ -2,11 +2,24 @@
 #
 _dapp()
 {
-  if [ "${#COMP_WORDS[@]}" != "2" ]; then
-    return
-  fi
+  local _commands cur prev
+  COMREPLY=()
+  _commands="$( grep -E '^[a-zA-Z_-]+.*{.*?## .*$' bin/dapp | awk 'BEGIN { FS = "\\(.*?## " }; { printf $1 }' )"
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-  COMPREPLY=( $( compgen -W "$( grep -E '^[a-zA-Z_-]+.*{.*?## .*$' bin/dapp | awk 'BEGIN { FS = "\\(.*?## " }; { printf $1 }' )"  -- "${COMP_WORDS[1]}" ) )
-}
+  case "${prev}" in
+    build|desktop|info|rebuild|push|remove|run|root|shell|update|upgrade)
+#    'build' | 'desktop' | 'rebuild' | 'push' | 'remove' | 'run' | 'root' | 'shell' | 'update' | 'upgrade')
+      local dockerfiles=$( find /media/filer/os/dockerfiles/ -name Dockerfile | awk -F'/' '{print $(NF-1)}' )
+      COMPREPLY=( $( compgen -W "${dockerfiles}" -- "${cur}" ) )
+      return 0
+      ;;
+    *)
+      ;;
+  esac
+  
+  COMPREPLY=( $( compgen -W "${_commands}"  -- "${cur}" ) )
+} 
 
 complete -F _dapp dapp
