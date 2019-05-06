a78cb6c28063a3231c05e85617792571576a93c6
Ali Mustakim <alim@forwardcomputers.com>
2019-05-06 18:59:05 -0500

    updates [skip ci]

--- bin/dapp-completion.bash
+++ bin/dapp-completion.bash
@@ -4,7 +4,7 @@ _dapp()
 {
   local _commands cur prev
   COMREPLY=()
-  _commands="$( grep -E '^[a-zA-Z_-]+.*{.*?## .*$' bin/dapp | awk 'BEGIN { FS = "\\(.*?## " }; { printf $1 }' )"
+  _commands="$( grep -E '^[a-zA-Z_-]+.*{.*?## .*$' "${HOME}"/bin/dapp | awk 'BEGIN { FS = "\\(.*?## " }; { printf $1 }' )"
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD-1]}"
 
