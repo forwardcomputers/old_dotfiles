d8216aaac3b9df4c9cba5adfafecd1a9c61265e0
Ali Mustakim <alim@forwardcomputers.com>
2021-06-11 13:54:33 -0400

    updates [skip ci]

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,24 @@ module.exports = {
     browser: true,
     es2021: true
   },
+  plugins: [
+    'html',
+    'jsdoc'
+  ],
   extends: [
-    'standard'
+    'standard',
+    'plugin:jsdoc/recommended'
   ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module'
   },
   rules: {
-    "no-extra-boolean-cast": "off",
-    "no-unneeded-ternary": "off",
-    "array-bracket-spacing": ["error", "always"],
-    "computed-property-spacing": ["error", "always"],
-    "space-in-parens": ["error", "always"],
-    "template-curly-spacing": ["error", "always"]
+    'no-extra-boolean-cast': 'off',
+    'no-unneeded-ternary': 'off',
+    'array-bracket-spacing': [ 'error', 'always' ],
+    'computed-property-spacing': [ 'error', 'always' ],
+    'space-in-parens': [ 'error', 'always' ],
+    'template-curly-spacing': [ 'error', 'always' ]
   }
 }
